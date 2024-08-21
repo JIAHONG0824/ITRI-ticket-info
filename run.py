53dd39eff41b3d75276834fe296e5c4a92a56d38
XIE,JIA-HONG <64515959+JIAHONG0824@users.noreply.github.com>
2024-08-21 11:47:58 +0800

    Update run.py

--- run.py
+++ run.py
@@ -1,6 +1,5 @@
 from fastapi import FastAPI
 from gradio_ui import *
-import uvicorn
 
 app = FastAPI()
 
@@ -9,4 +8,3 @@ async def root():
     return 'Gradio app is running at /gradio', 200
 
 app = gr.mount_gradio_app(app, demo, path='/gradio')
-uvicorn.run(app)
