3889adc7bb0b7ce9c67d94c8dbee95cd2fbab165
XIE,JIA-HONG <0701phantom@gmail.com>
2024-08-25 17:59:35 +0800

    remove unnecessary function

--- gradio_ui.py
+++ gradio_ui.py
@@ -2,14 +2,7 @@ import openai
 import gradio as gr
 import base64
 import requests
-
-
-def openai_api(prompt, key):
-    openai.api_key = key
-    completion = openai.chat.completions.create(
-        model="gpt-4o", messages=[{"role": "user", "content": prompt}]
-    )
-    return completion.choices[0].message.content
+import os
 
 
 # Function to encode the image
@@ -19,68 +12,68 @@ def encode_image(image_path):
 
 
 def get_image_content(base64_image, api_key):
-    api_url = "https://api.openai.com/v1/chat/completions"
     headers = {"Content-Type": "application/json", "Authorization": f"Bearer {api_key}"}
     payload = {
-        "model": "gpt-4o",
+        "model": "gpt-4o-mini",
         "messages": [
+            {
+                "role": "system",
+                "content": """
+             請扮演一位臺灣人且你的專業是影像辨識專家，你需要達成以下目的
+             1.辨識圖片的車票類型是臺灣鐵路或台灣高鐵
+             台鐵的車種有:太魯閣、普悠瑪、自強、莒光、區間車
+             高鐵車票上不會特地標示車種，只會標示車次
+             回答時請回答"台鐵"或"高鐵"
+             2.辨識車票的日期
+             3.辨識車票的車次
+             4.辨識車票的出發時間-抵達時間
+             5.辨識車票的出發站-抵達站
+             """,
+            },
             {
                 "role": "user",
                 "content": [
                     {
                         "type": "text",
-                        "text": 
-                        """
-                        扮演影像識別專家，幫我把所有的細節都識別出來。
-                        請找出交通工具種類(台鐵或高鐵)，車次，日期，出發和抵達時間，出發站和終點站。
-                        """,
+                        "text": """
+                    請辨識圖片的車票，並整理成以下格式:
+                    範例如下\n
+                    車種:高鐵
+                    日期:2022/10/24
+                    車次:125
+                    出發站 出發時間 → 抵達站 抵達時間:台中 11:20 → 台北 13:05
+                    """,
                     },
                     {
                         "type": "image_url",
                         "image_url": {"url": f"data:image/jpeg;base64,{base64_image}"},
                     },
                 ],
-            }
+            },
         ],
+        "max_tokens": 300,
     }
-
-    try:
-        response = requests.post(api_url, headers=headers, json=payload)
-        response.raise_for_status()  # 如果發生HTTP錯誤，則會引發HTTPError異常
-        image_content = response.json()["choices"][0]["message"]["content"]
-        return image_content
-    except Exception as err:
-        print(f"Other error occurred: {err}")
-        return get_image_content(base64_image)
+    response = requests.post(
+        "https://api.openai.com/v1/chat/completions", headers=headers, json=payload
+    )
+    print(response.json()["choices"][0]["message"]["content"])
+    return response.json()["choices"][0]["message"]["content"]
 
 
 def image_recognition(image, api_key):
     base64_image = encode_image(image)
-    image_content = get_image_content(base64_image, api_key)
-    prompt = (
-        """
-        扮演文字處理專家，幫我把逐字稿整理成格式：\
-        原則2：輸出格式(但不用出現本句)：$type:| 車種 $train no.:| 車次 $date:| 日期 $time:| 出發時間-抵達時間 $station:| 出發站-抵達站\
-        原則3：Let's work this out in a step-by-step way to be sure we have the right answer.\
-        原則4：以繁體中文來命題。逐字稿： 
-        """
-        + image_content
-    )
-    result = openai_api(prompt, api_key)
-    print(result)
-    print("===")
-    qtype = result.split("$type:|")[1].split("$train no.:|")[0].strip()
-    print(qtype)
-    qtrain = result.split("$train no.:|")[1].split("$date:|")[0].strip()
-    print(qtrain)
-    qdate = result.split("$date:|")[1].split("$time:|")[0].strip()
-    print(qdate)
-    qtime = result.split("$time:|")[1].split("$station:|")[0].strip()
-    print(qtime)
-    qstation = result.split("$station:|")[1].strip()
-    print(qstation)
+    os.environ["OPENAI_API_KEY"] = api_key
 
-    return qtype, qtrain, qdate, qtime, qstation
+    while True:
+        image_content = get_image_content(base64_image, api_key)
+        qtype=image_content.split("\n")[0][3:5]
+        qdate=image_content.split("\n")[1][3:]
+        qtrain=image_content.split("\n")[2][3:]
+        qtime=image_content.split("\n")[3][20:]
+        if qtype == "台鐵" or qtype == "高鐵":
+            break
+    return qtype, qtrain, qdate, qtime
+
 
 
 with gr.Blocks() as demo:
@@ -96,11 +89,10 @@ with gr.Blocks() as demo:
             qtype = gr.Textbox(label="車種", value="")
             qtrain = gr.Textbox(label="車次", value="")
             qdate = gr.Textbox(label="日期", value="")
-            qtime = gr.Textbox(label="出發時間-抵達時間", value="")
-            qstation = gr.Textbox(label="出發站-抵達站", value="")
+            qstation_qtime = gr.Textbox(label="出發站 出發時間 → 抵達站 抵達時間", value="")
 
     submit_button.click(
         image_recognition,
         inputs=[file_input, api_key_input],
-        outputs=[qtype, qtrain, qdate, qtime, qstation],
+        outputs=[qtype, qtrain, qdate, qstation_qtime],
     )
